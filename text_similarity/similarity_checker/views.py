d1a90b7a1f6db23e2083e7f1964f7206d9d0f7e4
rsakao <reiji.sakao@gmail.com>
2024-02-07 13:26:22 +0000

    入力文字をカウントする簡易的なフォームを実装

--- text_similarity/similarity_checker/views.py
+++ text_similarity/similarity_checker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    text = ''  # デフォルトで空のテキスト
+    count = -1  # デフォルトでカウントを-1にしておき、フォーム未送信時には表示しない
+    if request.method == "POST":
+        text = request.POST.get('text', '')
+        count = len(text)
+    return render(request, 'similarity_checker/index.html', {'text': text, 'count': count})
