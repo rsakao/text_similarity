25893cf77e0cd5a48dcc9e3f48585a5453a4ff8a
rsakao <reiji.sakao@gmail.com>
2024-02-08 12:58:54 +0000

    bing search apiを使って文字列を検索する

--- text_similarity/similarity_checker/views.py
+++ text_similarity/similarity_checker/views.py
@@ -1,21 +1,46 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
+from pprint import pprint
 import json
+import requests
 from openai import OpenAI
 
 def index(request):
     text = ''
+    search_results = {}
     response_json = {}
     if request.method == "POST":
+        print("request start")
         text = request.POST.get('text', '')
+
+        # bing search
+        subscription_key = os.environ['BING_SEARCH_V7_SUBSCRIPTION_KEY']
+        endpoint = os.environ['BING_SEARCH_V7_ENDPOINT'] + "/v7.0/search"
+        
+        # 検索パラメータ
+        mkt = 'ja-JP'
+        params = {'q': text, 'mkt': mkt}
+        headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+
+        try:
+            response = requests.get(endpoint, headers=headers, params=params)
+            response.raise_for_status()
+            
+            # 検索結果を変数に格納
+            search_results = response.json()
+            limit_results = [{'name': item['name'], 'snippet': item['snippet'], 'url': item['url']} for item in search_results["webPages"]["value"]]
+            pprint(limit_results)
+        except Exception as ex:
+            print(ex)
+
+        # OpneAI API
         client = OpenAI()
 
         system_prompt_prompt_file_path = os.path.join(settings.BASE_DIR, 'similarity_checker', 'prompt', 'system')
         with open(system_prompt_prompt_file_path, 'r') as file:
             system_prompt = file.read().strip()
 
-        print("request start")
         completion = client.chat.completions.create(
             model="gpt-4-turbo-preview",
             messages=[
@@ -31,5 +56,5 @@ def index(request):
         print(response_json)
         print(type(response_json))
 
-    return render(request, 'similarity_checker/index.html', {'text': text, 'response_data': response_json})
+    return render(request, 'similarity_checker/index.html', {'text': text, 'search_results': limit_results, 'response_data': response_json})
 
